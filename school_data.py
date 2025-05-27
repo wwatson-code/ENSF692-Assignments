53a6f47d1103012a9cb6f3ff5ae7c0d1470ad584
wwatson-code <william.em.watson@gmail.com>
2025-05-27 09:05:07 -0600

    Add files via upload
    Initial files, first commit

---
+++ school_data.py
@@ -0,0 +1,35 @@
+# school_data.py
+# AUTHOR NAME
+#
+# A terminal-based application for computing and printing statistics based on given input.
+# You must include the main listed below. You may add your own additional classes, functions, variables, etc. 
+# You may import any modules from the standard Python library.
+# Remember to include docstrings and comments.
+
+
+import numpy as np
+from given_data import year_2013, year_2014, year_2015, year_2016, year_2017, year_2018, year_2019, year_2020, year_2021, year_2022
+
+# Declare any global variables needed to store the data here
+
+
+# You may add your own additional classes, functions, variables, etc.
+
+
+def main():
+    print("ENSF 692 School Enrollment Statistics")
+
+    # Print Stage 1 requirements here
+
+    # Prompt for user input
+
+    # Print Stage 2 requirements here
+    print("\n***Requested School Statistics***\n")
+
+    # Print Stage 3 requirements here
+    print("\n***General Statistics for All Schools***\n")
+
+
+if __name__ == '__main__':
+    main()
+
