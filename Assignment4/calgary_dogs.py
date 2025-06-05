163f16c82a1676c718a485523e3fb6bc23a2234c
William Watson <williamwatson@williams-laptop.ucalgary.ca>
2025-06-05 08:54:58 -0600

    ititial files for Assignment 4

---
+++ Assignment4/calgary_dogs.py
@@ -0,0 +1,21 @@
+# calgary_dogs.py
+# AUTHOR NAME
+#
+# A terminal-based application for computing and printing statistics based on given input.
+# Detailed specifications are provided via the Assignment 4 README file.
+# You must include the main listed below. You may add your own additional classes, functions, variables, etc. 
+# You may import any modules from the standard Python library.
+# Remember to include docstrings and comments.
+
+def main():
+
+    # Import data here
+
+    print("ENSF 692 Dogs of Calgary")
+
+    # User input stage
+
+    # Data anaylsis stage
+
+if __name__ == '__main__':
+    main()
