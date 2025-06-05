3f022376edb4624241b0100a3297881b34285dea
William Watson <williamwatson@Williams-Laptop.local>
2025-06-05 12:56:24 -0600

    implemented user input with error handling

--- Assignment4/calgary_dogs.py
+++ Assignment4/calgary_dogs.py
@@ -1,5 +1,5 @@
 # calgary_dogs.py
-# AUTHOR NAME
+# William Watson
 #
 # A terminal-based application for computing and printing statistics based on given input.
 # Detailed specifications are provided via the Assignment 4 README file.
@@ -7,15 +7,50 @@
 # You may import any modules from the standard Python library.
 # Remember to include docstrings and comments.
 
+import pandas as pd
+
+
+def get_user_input(all_data):
+    input_string = input("Please enter a dog breed: ")
+    input_string = input_string.upper()
+
+    if input_string in all_data['Breed'].values:
+        return input_string
+    else:
+        raise KeyError("Dog breed not found in data. Please try again.")
+
+
+
+
 def main():
 
     # Import data here
+    all_data = pd.read_excel("/Users/williamwatson/VScode-projects/ENSF692/ENSF692-Assignments/Assignment4/CalgaryDogBreeds.xlsx")
+    print(all_data)
+    
+
 
     print("ENSF 692 Dogs of Calgary")
 
     # User input stage
+    # Prompt for user input
+    while True:
+        try:
+            breed = get_user_input(all_data)
+            print("good input")
+            break
+        except KeyError as err:
+            print(err)
+            continue
 
     # Data anaylsis stage
 
+    # print years where breed 
+    breed_mask = all_data['Breed'] == breed
+    breed_years = all_data['Year'][breed_mask].unique()
+    print(breed_years)
+
+
 if __name__ == '__main__':
+    
     main()
